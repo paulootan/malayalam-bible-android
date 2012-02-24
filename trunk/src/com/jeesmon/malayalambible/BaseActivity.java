24980bee09ff2c854f855c0dc36384c78155ee4f
Jeesmon <Jeesmon@4a34a3a2-5e78-9d57-db38-c20318256b95>
2012-02-24 20:46:34 +0000

    v1.3

--- trunk/src/com/jeesmon/malayalambible/BaseActivity.java
+++ trunk/src/com/jeesmon/malayalambible/BaseActivity.java
@@ -1,13 +1,20 @@
 package com.jeesmon.malayalambible;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.jeesmon.malayalambible.utils.Constants;
+
 public class BaseActivity extends Activity {
+	protected static final int OPEN_BOOKMARKS_ACTIVITY = 0;
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    MenuInflater inflater = getMenuInflater();
@@ -24,6 +31,9 @@ public class BaseActivity extends Activity {
 	    case R.id.preferences:
 	    	showPrefenceActivity(this);
 	        return true;
+	    case R.id.bookmarks:
+	    	openBookmarksActivity(this);
+	        return true;
 	    default:
 	        return super.onOptionsItemSelected(item);
 	    }
@@ -37,5 +47,69 @@ public class BaseActivity extends Activity {
 	protected void showPrefenceActivity(Context context) {
 		Intent prefView = new Intent(context, AppPreferencesActivity.class);
 		startActivity(prefView);
+	}
+	
+	public void openBookmarksActivity(Context context) {
+		Intent i = new Intent(context, BookmarksListActivity.class);
+    	startActivityForResult(i, OPEN_BOOKMARKS_ACTIVITY);
+	}
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == OPEN_BOOKMARKS_ACTIVITY) {
+        	Book book = getBookFromBookmark(intent);
+        	
+        	if(book != null) {
+	        	finish();
+	        	
+	        	Intent chapterView = new Intent(this, ChapterViewActivity.class);
+				chapterView.putExtra("com.jeesmon.malayalambible.Book", book);
+				startActivity(chapterView);
+        	}
+        }
+	}
+
+	protected Book getBookFromBookmark(Intent intent) {
+		Book book = null;
+		if (intent != null) {
+			Bundle b = intent.getExtras();
+			if (b != null) {
+				String url = b.getString(Constants.EXTRA_ID_BOOKMARK);
+				try {
+					String[] array = url.split(":");
+					int bid = Integer.parseInt(array[0]);
+					int cid = Integer.parseInt(array[1]);
+					ArrayList<Integer> verseIds = null;
+					if(array.length == 3) {
+						verseIds = new ArrayList<Integer>();
+						String[] vs = array[2].split(",");
+						for(String v: vs) {
+							try {
+		    					if(v.indexOf('-') == -1) {
+		    						verseIds.add(Integer.parseInt(v));
+		    					}
+		    					else {
+		    						String[] se = v.split("-");
+		    						for(int i=Integer.parseInt(se[0]); i<=Integer.parseInt(se[1]); i++) {
+		    							verseIds.add(i);
+		    						}
+		    					}
+							}
+							catch(Exception e){}
+						}
+					}
+					
+					ArrayList<Book> books = Utils.getBooks();
+			    	if(bid > 0 && cid > 0) {
+			    		book = books.get(bid - 1);
+			    		book.setSelectedChapterId(cid);
+			    		book.setSelectedVerseIds(verseIds);
+			    	}
+				}
+				catch(Exception e){}
+			}
+		}
+		
+		return book;
 	}
 }
--- trunk/src/com/jeesmon/malayalambible/DataBaseAdapter.java
+++ trunk/src/com/jeesmon/malayalambible/DataBaseAdapter.java
@@ -1,5 +1,7 @@
 package com.jeesmon.malayalambible;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
@@ -34,5 +36,43 @@ public class DataBaseAdapter {
 		return database.query(table, new String[] { "verse_id", "verse_text"}, 
 				"book_id = ? AND chapter_id = ?", new String[]{bookId + "", chapterId + ""}, null,
 				null, null);
-	}	
+	}
+	
+	public Cursor fetchVerses(int bookId, int chapterId, String table, ArrayList<String> verses, char lang) {
+		StringBuilder q = new StringBuilder("SELECT verse_id, verse_text FROM ");
+		q.append(table).append(" WHERE ");
+		q.append("book_id = ? AND chapter_id = ? AND verse_id IN (");
+		if(verses != null) {
+			int count = 0;
+			for(String v : verses) {
+				if(v.charAt(0) == lang) {
+					if(count++ > 0) {
+						q.append(",");
+					}
+					q.append(v.substring(1));
+				}
+			}
+		}
+		q.append(") order by verse_id");
+		
+		return database.rawQuery(q.toString(), new String[]{bookId + "", chapterId + ""});
+	}
+	
+	public Cursor fetchVerses(int bookId, int chapterId, String table, ArrayList<Integer> verses) {
+		StringBuilder q = new StringBuilder("SELECT verse_id, verse_text FROM ");
+		q.append(table).append(" WHERE ");
+		q.append("book_id = ? AND chapter_id = ? AND verse_id IN (");
+		if(verses != null) {
+			int count = 0;
+			for(Integer v : verses) {
+				if(count++ > 0) {
+					q.append(",");
+				}
+				q.append(v);
+			}
+		}
+		q.append(") order by verse_id");
+		
+		return database.rawQuery(q.toString(), new String[]{bookId + "", chapterId + ""});
+	}
 }
