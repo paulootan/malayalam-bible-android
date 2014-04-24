31b018f3b538f4eb50ec2fabf87d43887bf9bdeb
anishckumar@gmail.com <anishckumar@gmail.com@4a34a3a2-5e78-9d57-db38-c20318256b95>
2014-04-24 16:06:09 +0000

    Beta version for Android 4.2 and above

---
+++ branches/malayalam-bible-android-v15-Beta/src/com/jeesmon/malayalambible/service/FontService.java
@@ -0,0 +1,52 @@
+package com.jeesmon.malayalambible.service;
+
+
+
+
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.Log;
+
+public class FontService  {
+	
+	 private static FontService Obj;
+	 
+	 private int currentapiVersion=0;
+	 private String custom_font=null;
+	 private String font_path=null;
+	 
+	 private FontService(){
+		 
+	 }
+	 
+	 public static FontService getInstance(){
+	        if(Obj == null){
+	        	Obj = new FontService();
+	        }
+	        return Obj;
+	    }
+	 
+	 public Typeface getTypeface(AssetManager mgr){
+		 
+		 
+		  Typeface tf=null;
+	      currentapiVersion = android.os.Build.VERSION.SDK_INT;
+		  Log.d("tag", "currentapi version"+currentapiVersion);
+		 /*checking current api version and assigns font based on it */
+		 if(currentapiVersion>=16){
+			 custom_font="NotoSansMalayalam.ttf";
+			 Log.d("tag", "notosans");
+		 }
+		 if(currentapiVersion<16){
+			 custom_font="AnjaliOldLipi.ttf";
+			 Log.d("tag", "anjali..");
+		 }
+		 font_path="fonts/"+custom_font;
+		 tf=Typeface.createFromAsset(mgr, font_path);
+         return tf;
+	 }
+	    }
+	     
+
+
